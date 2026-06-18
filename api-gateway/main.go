8836ac9027a5c5fe88e55a7e321583d1f1d9d408
Farhang-Osman <168280475+Farhang-Osman@users.noreply.github.com>
2026-06-18 14:11:57 +0100

    refactor

--- api-gateway/main.go
+++ api-gateway/main.go
@@ -22,7 +22,8 @@ import (
 const ( // gRPC service addresses
 	userServiceAddress      = "localhost:50051"
 	shortenerServiceAddress = "localhost:50052"
-	jwtSecret               = "your-secret-key-change-this-in-production" // Must match User Service
+	redisClientAddress      = "localhost:6379"
+	// jwtSecret               = "your-secret-key-change-this-in-production" // Must match User Service
 )
 
 type APIGateway struct {
@@ -292,7 +293,7 @@ func main() {
 	defer shortenerConn.Close()
 
 	rdb := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
+		Addr: redisClientAddress,
 	})
 
 	ctx := context.Background()
